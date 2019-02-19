2756d362725dd38414021b793cefe14dd2676ad5
Jens Christian Jensen <jensecj@gmail.com>
2019-02-19 20:26:02 +0100

    initial commit

---
+++ views.el
@@ -0,0 +1,28 @@
+;;; views.el --- easy views management. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019 Jens Christian Jensen
+
+;; Author: Jens Christian Jensen <jensecj@gmail.com>
+;; Keywords: views, workgroup
+;; Package-Version: 20190219
+;; Version: 0.1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'views)
