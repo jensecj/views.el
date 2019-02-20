3c9dddd7e401742df1898823d7f9811de86f3a4b
Jens Christian Jensen <jensecj@gmail.com>
2019-02-20 12:30:44 +0100

    add requires, save/load views to/from file

--- views.el
+++ views.el
@@ -1,10 +1,10 @@
-;;; views.el --- easy views management. -*- lexical-binding: t; -*-
+;;; views.el --- Save/restore window configurations. -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2019 Jens Christian Jensen
 
 ;; Author: Jens Christian Jensen <jensecj@gmail.com>
 ;; Keywords: views, workgroup
-;; Package-Version: 20190219
+;; Package-Version: 20190220
 ;; Version: 0.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -24,16 +24,44 @@
 
 ;;; Code:
 
-(setq views-file (concat user-emacs-directory "views.el"))
+(require 'dash)
+(require 'f)
+(require 'ht)
+(require 's)
 
-(defun views--add (name view))
-(defun views--remove (name))
+;;;;;;;;;;;;;;;;;;;;;;
+;; persisting views ;;
+;;;;;;;;;;;;;;;;;;;;;;
 
-(defun views--current-view ())
-(defun views--load-views ())
+(defvar views-file (concat user-emacs-directory "views.el"))
 
-(defun views-push () (interactive))
-(defun views-pop () (interactive))
+(defun views--save-views (views)
+  "Save VIEWS to `views-file'."
+  (unless (f-exists-p views-file)
+    (f-touch views-file))
+  (with-temp-file views-file
+    (prin1 views (current-buffer))))
+
+(defun views--load-views ()
+  "Load saved views from `views-file'."
+  (if (f-exists-p views-file)
+      (with-temp-buffer
+        (insert-file-contents views-file)
+        (or (read (current-buffer)) (ht)))
+    (message "views file '%s' not found" views-file)
+    (ht)))
+
+(defun views--add (name view)
+  "Add a view to the list of saved views."
+  (let ((views (views--load-views)))
+    (ht-set views name view)
+    (views--save-views views)))
+
+(defun views--remove (name)
+  "Remove a view by NAME, from the list of saved views."
+  (let ((views (views--load-views)))
+    (ht-remove views name)
+    (views--save-views views)))
 
 
 (provide 'views)
